d1c00a40b7a0ca85f798e3b81fcb84c1c27ea2b8
darshan s <darshans@snapwork.com>
2026-05-19 18:14:20 +0530

    index.js file chnages

--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,64 @@
 #!/usr/bin/env node
 
+console.log("NEW CLI VERSION RUNNING");
+
 const fs = require('fs-extra');
 const path = require('path');
+const os = require('os');
+const simpleGit = require('simple-git');
+
+const git = simpleGit();
 
 const command = process.argv[2];
-const skillName = process.argv[3];
+const repoUrl = process.argv[3];
 
-if (command === 'add') {
+const skillIndex = process.argv.indexOf('--skill');
+const skillName = process.argv[skillIndex + 1];
 
-    console.log(`Installing skill: ${skillName}`);
+console.log("Command:", command);
+console.log("Repo URL:", repoUrl);
+console.log("Skill Name:", skillName);
+
+async function installSkill() {
+
+    const tempDir = path.join(
+        os.tmpdir(),
+        'demo-skills-temp'
+    );
+
+    if (fs.existsSync(tempDir)) {
+        fs.removeSync(tempDir);
+    }
+
+    console.log('Cloning repository...');
+
+    await git.clone(repoUrl, tempDir);
+
+    console.log('Repository cloned');
 
     const source = path.join(
-        __dirname,
-        '../.github/skills',
+        tempDir,
+        '.github',
+        'skills',
         skillName
     );
 
-    console.log("Source Path:", source);
-
-    const destination = path.join(
-        process.cwd(),
-        '../.github/skills',
-        skillName
-    );
+    console.log('Source Path:', source);
 
     if (!fs.existsSync(source)) {
         console.log('Skill not found');
-        process.exit(1);
+        return;
     }
+
+    const destination = path.join(
+        process.cwd(),
+        '.skills',
+        skillName
+    );
 
     fs.copySync(source, destination);
 
     console.log('Skill installed successfully!');
 }
+
+installSkill();
